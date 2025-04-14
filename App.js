b99fe897b32be9d32c2ac572720306372a2905e6
dkay964 <dktofa@gmail.com>
2025-04-14 07:29:59 +0200

    src

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Forex Scanner App</h1>
+      <p>Welcome to the scanner!</p>
+    </div>
+  );
+}
+
+export default App;
